fa38ed2a3798f11060c023ce4230d977a91eb0eb
Jesus <jesus@thinkpad>
2026-06-29 09:46:03 +0200

    pdfs:rosenbrock: log-evidences up to 24 (4 by 4)

--- synth_inference_tests/pdfs/rosenbrock.py
+++ synth_inference_tests/pdfs/rosenbrock.py
@@ -3,10 +3,7 @@ Generalization of the Rosenbrock test function to arbitrary dimensions from
 https://arxiv.org/abs/1110.2997 (via https://arxiv.org/abs/2306.16923).
 
 Difficulty: pronounced curving degeneracies.
-
 """
-
-from warnings import warn
 
 import numpy as np
 
@@ -40,13 +37,17 @@ class Rosenbrock(PDF):
         return {
             # PolyChord (nlive=200d/num_repeats=50d/prec_crit=0.001), avg 20 realisations
             # 2: -5.824,  # +/- 0.025
-            2: -5.804,  # analytic, from arXiv:1110.2997
-            3: -10.47,  # +/- 0.03
+            2: -5.804,  # analytic, from arXiv:1110.2997; Ours: -5.825 +/- 0.025
+            3: -10.465,  # +/- 0.025
             4: -15.08,  # +/- 0.03
-            5: -19.67,  # +/- 0.03
-            6: -24.34,  # +/- 0.03
-            7: -28.92,  # +/- 0.03
-            8: -33.65,  # +/- 0.03
-            9: -38.37,  # +/- 0.04
-            10: -43.01,  # +/- 0.04; -43.2 +/- ~0.2, from arXiv:2306.16923
+            5: -19.69,  # +/- 0.03
+            6: -24.36,  # +/- 0.03
+            7: -28.935,  # +/- 0.035
+            8: -33.650,  # +/- 0.035
+            9: -38.410,  # +/- 0.035
+            10: -43.03,  # +/- 0.04; -43.2 +/- ~0.2, from arXiv:2306.16923
+            12: -52.47,  # +/- 0.04
+            16: -71.40,  # +/- 0.04
+            20: -90.44,  # +/- 0.04
+            24: -109.37,  # +/- 0.04
         }.get(self.dim)
